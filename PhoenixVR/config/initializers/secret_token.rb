6859dcb2aa66880aa49112a611a9b5044881d068
John Shaughnessy <johnfshaughnessy@gmail.com>
2014-01-07 14:27:28 -0800

    rails project added

---
+++ PhoenixVR/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PhoenixVR::Application.config.secret_key_base = 'f3eeacc60efee812e3e3c5e13b174c158ed0b588ea0570f8a0904b5c0bbcd4a12e0815991691bd3589d09b2a5cbe837182f8e2b5cd0fc8e1674d55cb11152043'
---
+++ PhoenixVR/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PhoenixVR::Application.config.session_store :cookie_store, key: '_PhoenixVR_session'
